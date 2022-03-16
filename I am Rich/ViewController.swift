a1d9a03e4721c0752086a6410721b92a1625e453
Eduardo Reis <eduardoreisdev@gmail.com>
2022-03-15 21:05:08 -0300

    WIP

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Eduardo Reis on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
